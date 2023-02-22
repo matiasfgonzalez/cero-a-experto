b11059a8a928079814aa737110e6dbeedd5a7aea
Matias Gonzalez <matiasgonzalez.652@gmail.com>
2023-02-21 21:53:48 -0300

    Se creo el componente Heading.

---
+++ atoms/Heading/Heading.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import "./Heading.css";
+
+const Heading = ({ children, color, size }) => (
+    <h1
+        className={classNames("heading", {
+            [`color-${color}`]: color,
+            [`size-${size}`]: size
+        })}
+    >
+        {children}
+    </h1>
+);
+
+Heading.propTypes = {
+    children: PropTypes.node.isRequired,
+    color: PropTypes.oneOf["primary"],
+    size: PropTypes.oneOf[("xs", "sm", "md")]
+};
+
+Heading.defaultProps = {
+    color: "default",
+    size: "md"
+};
+
+export default Heading;
---
+++ stories/atoms/Heading.stories.js
@@ -0,0 +1,11 @@
+import Heading from "../../atoms/Heading";
+
+export default {
+    title: "Atoms/Heading",
+    component: Heading
+};
+
+export const Default = () => <Heading>Heading</Heading>;
+export const Primary = () => <Heading color="primary">Heading</Heading>;
+export const Small = () => <Heading size="sm">Heading</Heading>;
+export const ExtraSmall = () => <Heading size="xs">Heading</Heading>;
